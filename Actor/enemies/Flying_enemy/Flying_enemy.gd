7c2a5d5d9556c50c47fc42df7642e7a86a92a5f5
Madhavaprasath <Madhavprasath088@gmail.com>
2021-05-21 06:41:10 +0530

    Bug Fixes

--- Actor/enemies/Flying_enemy/Flying_enemy.gd
+++ Actor/enemies/Flying_enemy/Flying_enemy.gd
@@ -9,8 +9,7 @@ onready var start_position=transform.get_origin().x
 
 
 
-
-var can_shoot=true
+var can_shoot=false
 func _ready():
 	flying=true
 
@@ -28,15 +27,17 @@ func attack_logic(delta):
 	if player:
 		direction=-sign(player.global_position.direction_to(global_position).x)
 		var distance=player.global_position.distance_to(global_position)
-		if distance>200:
+		if distance>110:
 			velocity=150*(global_position.direction_to(player.global_position))
-		elif distance<200:
-			velocity=Vector2(0,(player.global_position.y-global_position.y)-32)
-			if can_shoot:
-				animation_player.play("Shoot")
-				$Reload_timer.start(1)
-				can_shoot=false
+		elif distance<110:
+			velocity=Vector2(0,(player.global_position.y-global_position.y)-10)
+		if can_shoot:
+			animation_player.play("Shoot")
+			$Reload_timer.start(1)
+			can_shoot=false
 		body.scale.x=direction
+
+
 func _on_Reload_timer_timeout():
 	can_shoot=true
 
@@ -47,3 +48,11 @@ func shoot():
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if !can_shoot:
 		animation_player.play("Wander")
+
+
+func _on_Shoot_area_body_entered(body):
+	can_shoot=true
+
+
+func _on_Shoot_area_body_exited(body):
+	can_shoot=false
